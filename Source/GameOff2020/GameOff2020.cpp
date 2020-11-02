cea0ad08b8f01b897322d41e1cd2993a323f869e
Romuald <Romuald_auc@hotmail.fr>
2020-11-02 16:25:55 +0100

    [Add] Unreal Basic Top Down Project

---
+++ Source/GameOff2020/GameOff2020.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameOff2020.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameOff2020, "GameOff2020" );
+
+DEFINE_LOG_CATEGORY(LogGameOff2020)
+ 
